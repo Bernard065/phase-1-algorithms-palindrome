36ac48fa83cbd2640720774c6f7024e230fa1d4f
Bernard065 <bebenibernard@gmail.com>
2022-12-08 13:29:30 +0300

    isPalidrome string

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // iterate through the string
+  for (let i = 0; i < word.length / 2; i++) {
+    // compare the letter we're iterating over to the corresponding letter at the end of the string
+    const j = word.length - 1 - i;
+    if (word[i] !== word[j]) {
+      // if the letters don't match, return false
+      return false;
+    }
+  }
+  // if we reach the middle, and all the letters match, return true
+  return true;
 }
+
+
+    
 
 /* 
   Add your pseudocode here
-*/
+  when the string is reversed it return the same result as the original characters
+  
+  Iterate through the string comparing the letter over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+
+if we reach the middle, and all the letters match, return true
+  */
 
 /*
   Add written explanation of your solution here
